a6d86d726234e44bca39575ca3501e23f88306f6
printf-dj <dixitjhinkwan123@gmail.com>
2021-01-20 14:56:44 +0530

    Initial Commits for testing

--- js/hf.js
+++ js/hf.js
@@ -1,3 +1,27 @@
 $(function () {
-    $('#header').load('components/header.html')
+    $('.header').load('components/header.html')
+    $('#footer').load('components/footer.html')
+});
+
+//Hamburger//
+window.onload=function(){
+    const icon = document.querySelector('.hamburger');
+    icon.addEventListener('click', () => {
+        if (!icon.classList.contains('open')) {
+            icon.classList.add('open');
+        } else {
+            icon.classList.remove('open');
+        }
+    });
+    }
+
+//Sticky Header//
+window.addEventListener("scroll", (e) => {
+    const header = document.querySelector("header");
+    if (window.pageYOffset > 25) {
+      header.classList.add("sticky");
+    }
+    else {
+        header.classList.remove("sticky");
+    }
 });
