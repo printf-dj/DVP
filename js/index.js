a1590047583b05c6be24b69369f850cbd32a0c8e
printf-dj <dixitjhinkwan123@gmail.com>
2021-01-23 13:38:22 +0530

    Major features

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
           const dvp = { lat: 30.45520838256879, lng: 79.27242999999999 };
           // The map, centered at DVP
           const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 4,
+            zoom: 9,
             center: dvp,
           });
           // The marker, positioned at DVP
