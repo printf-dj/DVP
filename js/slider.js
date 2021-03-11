617b078c066c042495c1f220d9adfef2bd5b5d64
printf-dj <dixitjhinkwan123@gmail.com>
2021-03-11 13:29:32 +0530

    Image slider added

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var captions = document.getElementsByClassName("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i <slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i <captions.length; i++) {
+    captions[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  captions[slideIndex-1].style.display = "block";
+}
